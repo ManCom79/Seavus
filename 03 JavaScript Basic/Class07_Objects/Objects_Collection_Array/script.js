ef2ca9e8ccae9ece201507dccea675d9276958bf
Sasha Stamenkovski <prof.menko@gmail.com>
2023-12-20 16:32:05 +0100

    Object collection task finished

---
+++ 03 JavaScript Basic/Class07_Objects/Objects_Collection_Array/script.js
@@ -0,0 +1,30 @@
+let students = []
+
+let button = document.getElementById("button")
+
+button.addEventListener("click", function() {
+    let firstName = document.getElementById("firstName").value
+    let lastName = document.getElementById("lastName").value
+    let age = document.getElementById("age").value
+    console.log(firstName, lastName, age);
+    let newStudent = new student(firstName, lastName, age)
+    console.log(newStudent);
+    students.push(newStudent)
+    console.log(students);
+    firstName = document.getElementById("firstName").value = ""
+    lastName = document.getElementById("lastName").value = ""
+    age = document.getElementById("age").value = ""
+    return students
+})
+
+function student (name, surname, age) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age
+} 
+
+// let student = {
+//     name: "",
+//     surname: "",
+//     yearsOld: "" 
+// }
