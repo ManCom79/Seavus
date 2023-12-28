98d884afaa659ec0abb2cdf9e75d18d1a8a9635d
Sasha Stamenkovski <prof.menko@gmail.com>
2023-12-28 14:10:07 +0100

    Hangman RC1

--- 03 JavaScript Basic/WinterProject/Hangman/script.js
+++ 03 JavaScript Basic/WinterProject/Hangman/script.js
@@ -3,7 +3,17 @@ let playTvshows = document.getElementById("playTvshows");
 let keyboard = document.getElementById("keyboard");
 let categoryDiv = document.getElementById("categoryDiv");
 let livesCount = document.getElementById("lives");
-let gameStatus = "Playing";
+let hintButton = document.getElementById("hintButton")
+let playAgainButton = document.getElementById("playAgainButton")
+let usedLetters = [];
+let guessTitles = []; //will store the titles of the movies or tv shows, based on user's choice
+let result = "";
+let title = "";
+let lives = 10;
+let hints = []
+let category = "";
+let wordRow = document.getElementById("row");
+let hintCounter = 0
 
 let movies = [
   { title: "top gun", hint1: "Goose", hint2: "Tom Cruise" },
@@ -17,23 +27,14 @@ let movies = [
 ];
 
 let tvShows = [
-  { title: "Friends", hint1: "Unagi", hint2: "Chandler" },
+  { title: "friends", hint1: "Unagi", hint2: "Chandler" },
   {
-    title: "Two and a half man",
+    title: "two and a half man",
     hint1: "Charlie Sheen",
     hint2: "Ashton Kutcher",
   },
-  { title: "Big bang theory", hint1: "Sheldon", hint2: "Bazinga" },
+  { title: "big bang theory", hint1: "Sheldon", hint2: "Bazinga" },
 ];
-
-let usedLetters = [];
-
-let guessTitles = []; //will store the titles of the movies or tv shows, based on user's choice
-let result = "";
-let title = "";
-let lives = 10;
-let category = "";
-let wordRow = document.getElementById("row");
 
 //Enable keyboard and fill in the array of movies titles
 playMovies.addEventListener("click", function () {
@@ -65,10 +66,18 @@ playTvshows.addEventListener("click", function () {
   getWordToGuess();
 });
 
-// Show dashes for each letter of the title of the show on the screen
+// Show dashes for each letter of the title the screen and filles in hints array for the title
 function getWordToGuess() {
   let selectRandomTitle = Math.floor(Math.random() * movies.length);
   randomTitle = guessTitles[selectRandomTitle];
+  if (category === "Movies") {
+    hints.push(movies[selectRandomTitle].hint1)
+    hints.push(movies[selectRandomTitle].hint2)
+  }
+  if (category === "TV Shows") {
+    hints.push(tvShows[selectRandomTitle].hint1)
+    hints.push(tvShows[selectRandomTitle].hint2)
+  }
   console.log(`Random title is ${randomTitle}`);
   for (let i = 0; i < randomTitle.length; i++) {
     if (randomTitle[i] !== " ") {
@@ -80,30 +89,68 @@ function getWordToGuess() {
   return randomTitle;
 }
 
-letterclick = addEventListener("click", function (event) {
-  let result = event.target.innerHTML;
-  if (result.length == 1) {
-    if (!usedLetters.includes(result)) {
-      for (let i = 0; i < randomTitle.length; i++) {
-        if (randomTitle[i] === result) {
-          let line = this.document.getElementById(`${i}`);
-          line.innerText = result;
+//catch event and process logic on clicked letters from app keyboard
+letterclick = addEventListener(
+  "click",
+  function (event) {
+    let result = event.target.innerHTML;
+    if (result.length == 1) {
+      if (!usedLetters.includes(result)) {
+        for (let i = 0; i < randomTitle.length; i++) {
+          if (randomTitle[i] === result) {
+            let line = this.document.getElementById(`${i}`);
+            line.innerText = result;
+          }
+        }
+        let unresolvedLetters = [];
+        for (let i = 0; i < randomTitle.length; i++) {
+          let catchDash = document.getElementById(`${i}`);
+          if (catchDash !== null) {
+            if (catchDash.innerText === "-") {
+              unresolvedLetters.push(catchDash.innerText);
+            }
+          }
+        }
+        if (unresolvedLetters.length === 0) {
+          this.setTimeout(function() {alert("Victory!")}, "10");
+        }
+        if (randomTitle.includes(result) === false) {
+          if (lives > 0) {
+            console.log(`${result} is not in the ${randomTitle}`);
+            lives = lives - 1;
+            livesCount.innerText = `You have ${lives} lives left`;
+            console.log(`${lives} lives remaining`);
+          } else {
+            this.alert("GAME OVER!!!");
+          }
         }
       }
-      if (randomTitle.includes(result) === false) {
-        if (lives > 0) {
-          console.log(`${result} is not in the ${randomTitle}`);
-          lives = lives - 1;
-          livesCount.innerText = `You have ${lives} lives left`;
-          console.log(`${lives} lives remaining`);
-        } else {
-          this.alert("GAME OVER!!!");
-        }
-      }
+      usedLetters.push(result);
+      console.log(usedLetters);
+    } else {
+      console.log(`${result} is already in array ${usedLetters}`);
     }
-    usedLetters.push(result);
-    console.log(usedLetters);
-  } else {
-    console.log(`${result} is already in array ${usedLetters}`);
+  }  
+);
+
+//Showing hints when Hints button is clicked
+hintButton.addEventListener("click", function(){
+  let hintBox = document.getElementById("hintBox")
+  if (hintCounter === 0) {
+    hintBox.innerText = hints[0]
+    hintCounter = hintCounter + 1
   }
-});
+  else if (hintCounter === 1) {
+      hintBox.innerText = hints[1]
+      hintCounter = hintCounter + 1
+  }
+  else {
+    hintBox.innerText = "No more hints!"
+  }
+  }
+)
+
+//Reload the page to start over
+playAgainButton.addEventListener("click", function() {
+  location.reload()
+})
