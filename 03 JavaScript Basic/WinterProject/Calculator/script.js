8cbc9b51fcb8d0cdbdf0c8daeecd2e5a0b05ddc4
Sasha Stamenkovski <prof.menko@gmail.com>
2024-01-03 15:00:59 +0100

    calculator

---
+++ 03 JavaScript Basic/WinterProject/Calculator/script.js
@@ -0,0 +1,113 @@
+let result = null;
+let userNumberInput = "";
+let number = "";
+let calculationsContainer = [];
+
+let upperScreen = document.getElementById("upperScreen");
+let lowerScreen = document.getElementById("lowerScreen");
+
+//Evaluating expression based on values and operation pushed in array by event listeners
+function evaluateExpression() {
+  if (calculationsContainer[1] === "+") {
+    result = calculationsContainer[0] + calculationsContainer[2];
+    console.log(`Result is ${result}`);
+    calculationsContainer.shift();
+    calculationsContainer.shift();
+    calculationsContainer.shift();
+    calculationsContainer.unshift(result);
+    lowerScreen.innerText = result;
+    upperScreen.innerText = `${result} `;
+  } else if (calculationsContainer[1] === "-") {
+    result = calculationsContainer[0] - calculationsContainer[2];
+    console.log(`Result is ${result}`);
+    calculationsContainer.shift();
+    calculationsContainer.shift();
+    calculationsContainer.shift();
+    calculationsContainer.unshift(result);
+    lowerScreen.innerText = result;
+    upperScreen.innerText = `${result} `;
+  } else if (calculationsContainer[1] === "÷") {
+    if (calculationsContainer[2] === 0) {
+        lowerScreen.innerText = 'ERROR! Division by 0!'
+    } else {
+        result = calculationsContainer[0] / calculationsContainer[2];
+        console.log(`Result is ${result}`);
+        calculationsContainer.shift();
+        calculationsContainer.shift();
+        calculationsContainer.shift();
+        calculationsContainer.unshift(result);
+        lowerScreen.innerText = result;
+        upperScreen.innerText = `${result} ÷`;
+    }
+  } else if (calculationsContainer[1] === "×") {
+    result = calculationsContainer[0] * calculationsContainer[2];
+    console.log(`Result is ${result}`);
+    calculationsContainer.shift();
+    calculationsContainer.shift();
+    calculationsContainer.shift();
+    calculationsContainer.unshift(result);
+    upperScreen.innerText = `${result} ×`;
+    lowerScreen.innerText = result;
+  }
+}
+
+//listener
+listenInput = addEventListener("click", function (event) {
+    //listen clicks on numbers
+  if (event.target.className.includes("number")) {
+    userNumberInput += event.target.innerText;
+    number = userNumberInput;
+    if (upperScreen.innerText == "") {
+      upperScreen.innerText = number;
+    } else {
+      if (calculationsContainer.length === 2) {
+        upperScreen.innerText = `${calculationsContainer[0]} ${calculationsContainer[1]} ${number}`;
+      } else {
+        upperScreen.innerText = `${number}`;
+      }
+    }
+  }
+
+  //listen clicks on operations
+  if (event.target.className.includes("operation")) {
+    if (number === "") {
+      console.log("No number in number");
+    } else {
+      upperScreen.innerText = `${number} ${event.target.innerText}`;
+      calculationsContainer.push(parseFloat(number));
+      calculationsContainer.push(event.target.innerText);
+      number = "";
+      userNumberInput = "";
+      upperScreen.innerText = `${calculationsContainer[0]} ${event.target.innerText}`;
+      if (calculationsContainer.length > 3) {
+        upperScreen.innerText = `${calculationsContainer[0]} ${event.target.innerText}`;
+        evaluateExpression();
+        upperScreen.innerText = `${calculationsContainer[0]} ${event.target.innerText}`;
+      }
+    }
+  }
+
+  //equal button
+  if (event.target.className.includes("equal")) {
+    if (number === "") {
+    } else {
+      calculationsContainer.push(parseFloat(number));
+      number = "";
+      userNumberInput = "";
+      evaluateExpression();
+      upperScreen.innerText = "";
+      console.log("Equal is", result);
+      calculationsContainer = [];
+    }
+  }
+
+  //clear screen button
+  if (event.target.className.includes("clear")) {
+    result = null;
+    number = "";
+    userNumberInput = "";
+    calculationsContainer = [];
+    upperScreen.innerText = "";
+    lowerScreen.innerText = "";
+  }
+});
