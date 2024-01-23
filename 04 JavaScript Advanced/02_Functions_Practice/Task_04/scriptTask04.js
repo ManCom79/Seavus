a869f928cf03c02249de9637b0bb3b0a5c6230c1
Sasha Stamenkovski <prof.menko@gmail.com>
2024-01-23 21:40:19 +0100

    task 4 comments

--- 04 JavaScript Advanced/02_Functions_Practice/Task_04/scriptTask04.js
+++ 04 JavaScript Advanced/02_Functions_Practice/Task_04/scriptTask04.js
@@ -5,20 +5,20 @@ let btnSubmitValue = document.getElementById("btnSubmitValue")
 
 
 btnSubmitValue.addEventListener("click" , function() {
-    let inputNumber = document.getElementById("numberHolder").value
-    let factorielResult = document.getElementById("factorielResult")
+    let inputNumber = document.getElementById("numberHolder").value //get value
+    let factorielResult = document.getElementById("factorielResult") //get header
 
-    console.log(inputNumber);
+    // console.log(inputNumber);
 
-    let factoriel = (function self(number) {
+    let factoriel = (function self(number) { //assigning self invoked function to a variable
         if (number === 0) {
             return 1
         } else {
-            return number * self(number - 1)
+            return number * self(number - 1) //recursion
         }
     })(Number(inputNumber))
-    console.log(factoriel);
+    console.log(factoriel); //log result to console
 
-    factorielResult.innerHTML = `${factoriel}`
+    factorielResult.innerHTML = `${factoriel}` //write result to h1 holder
 })
 
